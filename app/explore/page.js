4976ac74544d99635f2c7b71927c1507847005be
unknown <kausiksikdar9@gmail.com>
2025-03-07 13:55:32 +0530

    8th commit

--- app/explore/page.js
+++ app/explore/page.js
@@ -200,7 +200,7 @@ export default function Explore() {
 
             <input
               type="text"
-              placeholder="Describe your post little bit..."
+              placeholder="Describe little bit for better understanding and storage..."
               value={prompt}
               onChange={(e) => setPrompt(e.target.value)}
               className="w-full p-3 border rounded-lg mt-4 bg-gray-50"
